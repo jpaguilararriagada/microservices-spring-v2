76881b4ae14509459250c1f577a42af4ca6d33ac
Juan Pablo Aguilar <jparriagadaaguilar@gmail.com>
2023-07-07 01:52:32 -0400

    se agrega limits-service

---
+++ limits-service/src/main/java/com/jota/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jota.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+//aaaaa
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
